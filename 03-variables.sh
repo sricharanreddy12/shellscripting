566ddbefc2c35ad162ec6c34dd3559d6bef2e5da
Sri Charan Reddy <charankudumurula@gmail.com>
2025-05-20 16:34:50 +0530

    hello world practicing

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+person1= modi
+person2= shah
+
+echo "$person1:: Hey $person2, hope you doing good"
+echo "$person2: Hlo bhai, doing good. how are you doing"
+echo "$person1:: I too good man. what are the meetings your having for today"
+echo "$person2: I have to visit the ahmedabad meeting  for today"
+echo "$person1: Thats sounds good $person2"
