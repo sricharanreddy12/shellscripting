7f9530c103eca788d36db77d8bc8abf4749d74a4
Sri Charan Reddy <charankudumurula@gmail.com>
2025-05-20 16:25:32 +0530

    hello world practicing

---
+++ 02-convrsation.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+
+echo "modi:: Hey shah, hope you doing good"
+echo "shah: Hlo bhai, doing good. how are you doing"
+echo "modi:: I too good man. what are the meetings your having for today"
+echo "shah: I have to visit the ahmedabad meeting  for today"
+echo "modi: Thats sounds good shah"
