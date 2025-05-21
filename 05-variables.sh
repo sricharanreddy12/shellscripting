7a45365d739c65d6e38b00c792282ea1dc9c6128
Sri Charan Reddy <charankudumurula@gmail.com>
2025-05-21 15:56:31 +0530

    shell practice

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,5 +2,5 @@
 
 echo "enter your pin number::"
 
-read PIN # here pin is a variable and read command for to ask pin number
+read-s PIN # here pin is a variable and read command for to ask pin number
 echo " your number is :$PIN"
