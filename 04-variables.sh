be93c163d954a881ecef1daf74a7f70c1c1384cf
Sri Charan Reddy <charankudumurula@gmail.com>
2025-05-21 15:38:04 +0530

    shell practice

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hey $PERSON2, hope you doing good"
+echo "$PERSON2: Hlo bhai, doing good. how are you doing"
+echo "$PERSON1:: I too good man. what are the meetings your having for today"
+echo "$PERSON2: I have to visit the ahmedabad meeting  for today"
+echo "$PERSON1: Thats sounds good $PERSON2"
