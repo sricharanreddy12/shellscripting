94bc46151a204494b3d8963a2610675098aa395f
Sri Charan Reddy <charankudumurula@gmail.com>
2025-05-21 16:33:09 +0530

    shell practice

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+movies=("syee" "bobby" "legend" "Hit3")
+
+echo "print the first movie :${movies[0]}"
+
+echo "print the second movie :${movies[3]}"
+
+echo "all movies :${movies{@}}"
